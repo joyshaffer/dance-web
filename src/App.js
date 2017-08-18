5a6ff7a7d9029bf4d4908c2dbc79e04253dd1c67
Joy Shaffer <jeshaffer@anderson.edu>
2017-08-18 14:42:39 -0500

    add react router and wrap app in router

--- src/App.js
+++ src/App.js
@@ -8,20 +8,7 @@ class App extends Component {
         <div className="App-header">
           <h3>Study For Your Dance Exams!</h3>
         </div>
-        <ul className="nav-links">
-          <li>
-            <NavLink to='/body-positions'>Positions of the Body</NavLink>
-          </li>
-          <li>
-            <NavLink to='/feet-positions'>5 Positions of the Feet And Arms</NavLink>
-          </li>
-          <li>
-            <NavLink to='/terms'>Terms</NavLink>
-          </li>
-          <li>
-            <NavLink to='/notes'>Extra Notes</NavLink>
-          </li>
-        </ul>
+        
         
       </div>
     )
