7d9d3a6352f456731b4d3ecbd4e59eb509d735e8
Joy Shaffer <jeshaffer@anderson.edu>
2017-08-18 14:52:57 -0500

    add header content and style it

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react'
 import './App.css'
+import { Route, Switch } from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <h3>Study For Your Dance Exams!</h3>
+          <div className="App-headings">
+            <h1>Study For Your Dance Exams!</h1>
+          </div>
         </div>
         
         
