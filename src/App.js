53d7d4409870f3d93db46e2bb43564a30cbbd738
Joy Shaffer <jeshaffer@anderson.edu>
2017-08-18 15:02:16 -0500

    add navlink and route for body positions

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import './App.css'
-import { Route, Switch } from 'react-router-dom'
+import { Route, Switch, NavLink } from 'react-router-dom'
 
 class App extends Component {
   render() {
@@ -10,9 +10,16 @@ class App extends Component {
           <div className="App-headings">
             <h1>Study For Your Dance Exams!</h1>
           </div>
+          <ul className="nav-links">
+            <li>
+              <NavLink to="/body-positions">Positions of the body</NavLink>
+            </li>
+          </ul>
         </div>
-        
-        
+        <Switch>
+          <Route path='/body-positions' render={() => <h1>Body Positions</h1>} />
+          <Route render={() => <p>"Dance is the hidden language of the soul of the body" - Martha Graham</p>} />
+        </Switch>
       </div>
     )
   }
